f342e82d581a486a7ac15d4a2ba55ba3488163c5
Takumi-Naka <49777735+Takumi-Naka@users.noreply.github.com>
2019-10-30 13:52:35 +0900

    webページにアクセスするとボタンが一番上に出てくる機能を追加

--- test_takumi/content.ts
+++ test_takumi/content.ts
@@ -1,7 +1,8 @@
 window.alert('アプリを開いた');
+//左クリックするとその位置にボタンが表示される
 document.addEventListener('mouseup', function (ev) {
     var selection = getSelection();
-    var h1Node = document.createElement('h1');
+    var h1Node = document.createElement('div');
     h1Node.innerHTML="<button type=\"button\">ボタン</button>";
     //var textNode = document.createTextNode("あ");
     if (selection.rangeCount > 0) {
@@ -10,3 +11,12 @@ document.addEventListener('mouseup', function (ev) {
         selection.removeAllRanges();
     }
 }, false);
+
+
+//ボタンがWebページにアクセスしたときに一番上に出てくる。
+var r = document.createRange();
+r.setStart(document.body,0);
+r.setEnd(document.body,0);
+var h2Node = document.createElement('div');
+h2Node.innerHTML="<button type=\"button\">ボタン</button>";
+r.insertNode(h2Node);
