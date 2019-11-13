3e033c339611a8b89cb21b2c1e05d5caf91ae440
平井 李音（ヒライ リオ、Hirai Rio） <49777540+Rio-Hirai@users.noreply.github.com>
2019-11-13 13:51:07 +0900

    Add files via upload

---
+++ mouse_test_1113_hriai/background.js
@@ -0,0 +1,8 @@
+chrome.contextMenus.create({
+    title: "コンテキストメニューを追加",
+    contexts: ["all"],
+    type: "normal",
+    onclick: function (info) {
+        window.alert(info.selectionText);
+    }
+});
