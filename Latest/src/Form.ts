e579232f783aa9ce3e4b5d102cbcf668720d4ff1
takuma hidaka <hidakatakuma.tukuba@gmail.com>
2019-12-06 16:35:15 +0900

    var->letに変換した

--- Latest/src/Form.ts
+++ Latest/src/Form.ts
@@ -13,49 +13,49 @@ class Form{
         }
 ​
         //ポップアップとして表示するもの全体のdivを用意
-        var latest_div = document.createElement("div");
+        let latest_div = document.createElement("div");
         latest_div.id = "latest_div";
         latest_div.style.backgroundColor = "#e6e6fa";
         document.body.appendChild(latest_div);
 ​
         // table作成
-        var latest_table = document.createElement("table");
+        let latest_table = document.createElement("table");
         latest_table.id = "latest_table";
         latest_div.appendChild(latest_table);
 
         //tableのbody作成
-        var latest_tbody = document.createElement("tbody");
+        let latest_tbody = document.createElement("tbody");
         latest_tbody.id = "latest_tbody";
         latest_table.appendChild(latest_tbody);
 
         //table 1行目作成
-        var latest_tr0 = latest_tbody.insertRow(-1);
+        let latest_tr0 = latest_tbody.insertRow(-1);
         latest_tr0.id = "latest_tr0";
         //tabel 1行目のheader作成。
-        var latest_th0= document.createElement("th");
+        let latest_th0= document.createElement("th");
         latest_th0.id = "latest_th0";
         latest_th0.textContent = "ユーザーネーム";
         latest_tr0.appendChild(latest_th0);
         //tabel 1行目の値を作成。
-        var latest_td0 = document.createElement("td");
+        let latest_td0 = document.createElement("td");
         latest_td0.id = "latest_td0";
-        var latest_input0: HTMLInputElement = <HTMLInputElement>document.createElement("input");
+        let latest_input0: HTMLInputElement = <HTMLInputElement>document.createElement("input");
         latest_input0.id = "input0";
         latest_td0 = <HTMLTableDataCellElement><unknown>latest_input0;
         latest_tr0.appendChild(latest_td0);
 
         //table 2行目作成
-        var latest_tr1 = latest_tbody.insertRow(-1);
+        let latest_tr1 = latest_tbody.insertRow(-1);
         latest_tr1.id = "latest_tr1";
         //table 2行目のheader作成。
-        var latest_th1 = document.createElement("th");
+        let latest_th1 = document.createElement("th");
         latest_th1.id = "latest_th1";
         latest_th1.textContent = "コメント";
         latest_tr1.appendChild(latest_th1);
         //table 2行目の値を作成。
-        var latest_td1 = document.createElement("td");
+        let latest_td1 = document.createElement("td");
         latest_td1.id = "latest_td1";
-        var latest_input1: HTMLTextAreaElement = <HTMLTextAreaElement>document.createElement("textarea");
+        let latest_input1: HTMLTextAreaElement = <HTMLTextAreaElement>document.createElement("textarea");
         latest_input1.id = "textarea";
         latest_td1 = <HTMLTableDataCellElement><unknown>latest_input1;
         latest_tr1.appendChild(latest_td1);
@@ -70,8 +70,8 @@ class Form{
             modal: true,
             buttons: {
                 "登録": function(){
-                var tmp_user: string = "";
-                var tmp_comment: string = "";
+                let tmp_user: string = "";
+                let tmp_comment: string = "";
                 tmp_user = latest_input0.value;
                 tmp_comment = latest_input1.value;
                 console.log("ユーザーネーム: "+tmp_user+"   コメント: "+tmp_comment);
